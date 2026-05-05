bf1e647e15eda77d7df2f8f00b9286f1be335e22
dadzerdous <dadzaverous@gmail.com>
2026-05-05 02:27:28 -0400

    Refactor actions parsing from dataset

--- render.js
+++ render.js
@@ -282,7 +282,7 @@ document.getElementById('log').addEventListener('click', e => {
   }
 
   const name    = obj.dataset.name;
-  const actions = JSON.parse(obj.dataset.actions || '[]');
+  const actions = (obj.dataset.actions || '').split('|').filter(Boolean);
   if (!name || !actions.length) return;
 
   _activeCtx = '__inv__';
